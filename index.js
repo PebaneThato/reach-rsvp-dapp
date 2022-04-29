c16037c6a76792f8f3a3e1d52146272e21d92032
Thato Pebane <jpebane@gmail.com>
2022-04-29 22:45:52 +0200

    Add Algorand support

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import './index.css';
 import * as backend from './build/index.main.mjs';
 import { loadStdlib } from '@reach-sh/stdlib';
 const reach = loadStdlib(process.env);
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from '@reach-sh/stdlib';
+reach.setWalletFallback(reach.walletFallback({ providerEnv: 'TestNet', MyAlgoConnect }));
 
 const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};
 const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
